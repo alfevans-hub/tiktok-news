7b030deecf2c78b1d9d82e645b7e9cd8e0e04342
alfevans-hub <alf.evans@icloud.com>
2026-05-04 22:53:59 +0100

    add TikTok domain verification file

---
+++ app/tiktoklTJlOv13pDQAdIbbklQVUif7zB6q6GRV.txt/route.ts
@@ -0,0 +1,5 @@
+export async function GET() {
+  return new Response('tiktok-developers-site-verification=lTJlOv13pDQAdIbbklQVUif7zB6q6GRV', {
+    headers: { 'Content-Type': 'text/plain' },
+  })
+}
